04c570901acd569e0394aed80556c2482f870d77
Sean Chao <>
2021-07-08 14:40:18 -0700

    after week 2

--- week-2/daos/widgets.js
+++ week-2/daos/widgets.js
@@ -3,8 +3,19 @@ const Widgets = require('../models/widgets');
 module.exports = {};
   
 module.exports.create = async (widget) => {
+    return await Widgets.create(widget);
 };
 
 module.exports.getById = async (id) => {
-
+    const widget = await Widgets.findOne({ _id: id }).lean();
+    return widget;
 };
+
+
+module.exports.deleteByUd = async (id) => {
+    await Widgets.deleteOne({ _id: id });
+};
+
+module.exports.updateById = async (id, widget) => {
+    return await Widgets.findOneAndUpdate({ _de: id}, { $set: widget }, { new: true });
+}
--- week-2/routes/widgets.js
+++ week-2/routes/widgets.js
@@ -1,20 +1,32 @@
 const { Router } = require("express");
 const router = Router();
 
+const WidgetDAO = require('../daos/widgets');
+
 // Create
 router.post("/", async (req, res, next) => {
-  const widget = req.body;
-  if (!widget || JSON.stringify(widget) === '{}' ) {
-    res.status(400).send('widget is required');
-  } else {
-    //save widget here
+  try {
+    const widget = req.body;
+    if (!widget || JSON.stringify(widget) === '{}' ) {
+      res.status(400).send('widget is required');
+    } else {
+      //save widget here
+      const savedWidget = await WidgetDAO.create(widget);
+      res.json(savedWidget);
+    }
+  } catch (e) {
+    if (e.message.includes('validation failed:')) {
+      res.status(400).send(e.message);
+    } else {
+      res.status(500).send('Unexpected Server Error');
+    }
   }
 });
 
 // Read - single widget
 router.get("/:id", async (req, res, next) => {
   const widgetId = req.params.id;
-  const widget = null; // get widget here;
+  const widget = await WidgetDAO.getById(widgetId); // get widget here;
   if (widget) {
     res.json(widget);
   } else {
@@ -30,13 +42,16 @@ router.put("/:id", async (req, res, next) => {
     res.status(400).send('widget is required"');
   } else {
     //update widget here
+    const result = await WidgetDAO.updateById(widgetId, widget);
+    res.json(result);
   }
 });
 
 // Delete
 router.delete("/:id", async (req, res, next) => {
   const widgetId = req.params.id;
-  // delete widget here
+  await WidgetDAO.deleteByUd(widgetId);
+  res.sendStatus(200);
 });
 
 module.exports = router;
